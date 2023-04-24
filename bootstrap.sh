cc18cff0044374f3bc31c2e0ec3cad47d89443ac
Alex Lemanski <bitfyre@Alexs-Luna-MBP.local>
2023-04-24 09:38:52 -0500

    Setting up addtional bash configs and git config
    Setup:
    
    * Git Config
    * Vimrc link
    * Additional Bash configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,4 +12,21 @@ ln -s $(pwd)/git/gitignore_global ${HOME}/git/.gitignore_global
 ln -s $(pwd)/git/gitconfig ${HOME}/.gitconfig
 
 # setup vim
+ln -s $(pwd)/vim/vimrc ${HOME}/.vimrc
 
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# setup homebrew
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> ${HOME}/.bash_profile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.bash_profile
+source ${HOME}/.bash_profile
+
+# install mas
+brew install mas
+
+# setup Brewfile
+ln -s $(pwd)/brew/Brewfile ${HOME}/.Brewfile
+
+# install other homebrew eackages
+brew bundle install --global
